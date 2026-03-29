8e811173c8b03a040e6ccdd37e72a206a23a97aa
jake <82261657+jake-yang@users.noreply.github.com>
2026-03-29 20:55:50 +0900

    Update send_telegram.py

--- send_telegram.py
+++ send_telegram.py
@@ -1,20 +1,38 @@
 import os
 import requests
+from datetime import datetime
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
+def get_prices():
+    url = "https://api.coingecko.com/api/v3/simple/price"
+    params = {
+        "ids": "bitcoin,tether",
+        "vs_currencies": "usd,krw"
+    }
 
-def send_message():
+    res = requests.get(url, params=params).json()
+
+    btc_usd = res["bitcoin"]["usd"]
+    usdt_krw = res["tether"]["krw"]
+
+    return btc_usd, usdt_krw
+
+def send_message(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-    res = requests.post(url, data={
+    requests.post(url, data={
         "chat_id": CHAT_ID,
-        "text": "🚀 GitHub Actions test success"
+        "text": text
     })
 
-    print(res.text)
-
-
 if __name__ == "__main__":
-    send_message()
+    btc, usdt = get_prices()
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    message = f"""⏰ {now}
+₿ BTC: ${btc:,}
+💱 USDT/KRW: ₩{usdt:,}
+"""
+
+    send_message(message)
